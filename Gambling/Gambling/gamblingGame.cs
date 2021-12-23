ff1eec339b7b1c258d92c89d13073650d6274ed2
Rajangane <pratiksharajangane7@gmail.com>
2021-12-23 10:19:35 +0530

    [Pratiksha] ADD : Winning or lossing the game

--- Gambling/Gambling/gamblingGame.cs
+++ Gambling/Gambling/gamblingGame.cs
@@ -7,13 +7,30 @@ using System.Threading.Tasks;
 namespace Gambling
 {
     public class gamblingGame
-    { 
-            public void Start()
+    {
+        int money = 100;
+        int bet = 1;
+        public void Start()
+        {
+            int win = 0;
+            int loss = 0;
+
+            Console.WriteLine("Total Money Is :" + money);
+            Random random = new Random();
+            int toss = random.Next(0, 2);
+            if (toss == 0)
             {
-                int money = 100;
-                int bet = 1;
-                Console.WriteLine("Total Money Is :" + money);
+                win += bet;
+                Console.WriteLine("Total Money after winning is {money + win} ");
             }
+            else if(toss == 1)
+            {
+                loss = bet;
+                Console.WriteLine("Total Money after lossing is {money + loss} ");
+            }
+
+        }
+
         
     }
 
